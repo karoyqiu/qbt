58e7dc6e69c7beae25771e49c2efa0d36f15bde0
Roy QIU <karoyqiu@gmail.com>
2024-08-01 11:16:44 +0800

    添加工具栏。

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,53 @@
+import { appWindow } from '@tauri-apps/api/window';
+import { IconField } from 'primereact/iconfield';
+import { InputIcon } from 'primereact/inputicon';
+import { InputText } from 'primereact/inputtext';
+import { Menubar } from 'primereact/menubar';
+import type { MenuItem } from 'primereact/menuitem';
+import { TabMenu } from 'primereact/tabmenu';
+import { useEffect } from 'react';
+
 function App() {
-  return <div>qBittorrent</div>;
+  const buttons: MenuItem[] = [
+    { label: 'Add', icon: 'pi pi-plus' },
+    { label: 'Pause', icon: 'pi pi-pause' },
+    { label: 'Resume', icon: 'pi pi-play' },
+    { label: 'Delete', icon: 'pi pi-trash' },
+    { label: 'Settings', icon: 'pi pi-cog' },
+  ];
+
+  useEffect(() => {
+    appWindow.show();
+  }, []);
+
+  return (
+    <>
+      <div className="card flex gap-4">
+        <Menubar className="border-none bg-transparent" model={buttons} />
+        <IconField className="grow self-center" iconPosition="left">
+          <InputIcon className="pi pi-search" />
+          <InputText className="w-full" type="search" placeholder="Search" />
+        </IconField>
+        <TabMenu
+          className="pb-1"
+          model={[
+            {
+              label: 'All',
+              icon: 'pi pi-list',
+            },
+            {
+              label: 'Downloading',
+              icon: 'pi pi-download',
+            },
+            {
+              label: 'Completed',
+              icon: 'pi pi-check',
+            },
+          ]}
+        />
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
+import { PrimeReactProvider } from 'primereact/api';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import 'primeicons/primeicons.css';
+
 import App from './App';
+import './app.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <PrimeReactProvider value={{ ripple: true }}>
+      <App />
+    </PrimeReactProvider>
   </React.StrictMode>,
 );
