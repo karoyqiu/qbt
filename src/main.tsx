eeb47616408a94b9a92522dad9f4f63cd9dac72b
Roy QIU <karoyqiu@gmail.com>
2024-08-02 16:55:14 +0800

    生产环境下禁用右键菜单。

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,14 @@ import 'primeicons/primeicons.css';
 import App from './App';
 import './app.css';
 
+if (!import.meta.env.DEV) {
+  document.addEventListener('contextmenu', (event) => {
+    if (!event.target || !('tagName' in event.target) || event.target.tagName !== 'INPUT') {
+      event.preventDefault();
+    }
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <PrimeReactProvider value={{ ripple: true, nonce: 'Eyb2JqlROQDm6V2LGWjuj' }}>
